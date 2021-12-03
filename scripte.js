d6e8551e69c0d27dacece6f4e7db9fbab96aa3e2
mehdi <mehdiutchiwa@gmail.com>
2021-12-04 00:06:53 +0100

    dark-mode-function SCARX-T-6

--- scripte.js
+++ scripte.js
@@ -1,1 +1,7 @@
-console.log("hey");
+const theme = document.getElementById("theme");
+
+theme.addEventListener("click", () => {
+  document.querySelector("body").classList = [
+    theme.checked ? "theme-light" : "theme-dark",
+  ];
+});
