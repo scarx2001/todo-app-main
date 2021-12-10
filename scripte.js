77659587c42312bdc6325c3a9b4b1eb64237ec7e
mehdi <mehdiutchiwa@gmail.com>
2021-12-10 08:38:38 +0100

    fixing-bugs

--- scripte.js
+++ scripte.js
@@ -1,67 +1,89 @@
-const theme = document.getElementById("theme");
-const newItemInput = document.getElementById("addItem");
 const todoList = document.querySelector(".content ul");
-const itemsLeft = document.querySelector(".items-left span");
 
-theme.addEventListener("click", () => {
-  document.querySelector("body").classList = [
-    theme.checked ? "theme-light" : "theme-dark",
-  ];
+// Theme Switcher
+const theme = document.getElementById("theme");
+theme.addEventListener("click", function switchThemeMode() {
+  const body = document.querySelector("body");
+  if (theme.checked) {
+    body.classList.add("theme-light");
+    body.classList.remove("theme-dark");
+  } else {
+    body.classList.remove("theme-light");
+    body.classList.add("theme-dark");
+  }
 });
+
+// Add new item
+const newItemInput = document.getElementById("addItem");
 newItemInput.addEventListener("keypress", (e) => {
-  if (e.charCode === 13 && newItemInput.value.length > 0) {
+  if (e.key === "Enter" && newItemInput.value.length > 0) {
     createNewTodoItem(newItemInput.value);
     newItemInput.value = "";
+    updateItemsCount("add");
   }
 });
 function createNewTodoItem(text) {
-  const elem = document.createElement("li");
-  elem.classList.add("flex-row");
-
-  elem.innerHTML = `
+  const todoItem = document.createElement("li");
+  todoItem.classList.add("flex-row");
+  todoItem.innerHTML = `
     <label class="list-item">
-    <input type="checkbox" class="checkbox" name="todoItem" checked />
-    <span class="checkmark"></span>
-    <span class="text">${text}</span>
+      <input type="checkbox" class="checkbox" name="todoItem"  />
+      <span class="checkmark"></span>
+      <span class="text">${text}</span>
     </label>
     <span class="remove"></span>
-  
-      `;
-  todoList.append(elem);
-  updateItemsCount(1);
+  `;
+
+  todoList.append(todoItem);
 }
-function updateItemsCount(number) {
-  itemsLeft.innerText = +itemsLeft.innerText + number;
+
+// Update the todo counter
+function updateItemsCount(action) {
+  const todoCounts = document.querySelectorAll(".todoCount");
+  let number = 0;
+  if (action === "add") {
+    ++number;
+  } else if (action === "remove") {
+    --number;
+  }
+
+  todoCounts.forEach((todoCount) => {
+    todoCount.innerText = Number(todoCount.innerText) + number;
+  });
 }
-//    remove items
-function removeTodoItem(elem) {
-  elem.remove();
-  updateItemsCount(-1);
+
+// Remove todo items
+function removeTodoItem(todo) {
+  todo.remove();
+  updateItemsCount("remove");
 }
 todoList.addEventListener("click", (event) => {
   if (event.target.classList.contains("remove")) {
     removeTodoItem(event.target.parentElement);
   }
 });
-document.querySelector(".clear").addEventListener("click", () => {
-  document.querySelectorAll(".list-item .checkbox:checked").forEach((item) => {
-    removeTodoItem(item.closest("li"));
+
+// Clear all todo items
+document.querySelectorAll(".clear").forEach((clearBtn) => {
+  clearBtn.addEventListener("click", () => {
+    document
+      .querySelectorAll(".list-item .checkbox:checked")
+      .forEach((item) => {
+        removeTodoItem(item.closest("li"));
+      });
   });
 });
-document.querySelector(".clear-completed").addEventListener("click", () => {
-  document.querySelectorAll(".list-item .checkbox:checked").forEach((item) => {
-    removeTodoItem(item.closest("li"));
-  });
-});
+
+// Filter todo items
 document.querySelectorAll(".filter input").forEach((radio) => {
   radio.addEventListener("change", (e) => {
     filterTodoItems(e.target.id);
   });
 });
-function filterTodoItems(id) {
+function filterTodoItems(status) {
   const allItems = todoList.querySelectorAll("li");
 
-  switch (id) {
+  switch (status) {
     case "all":
       allItems.forEach((item) => {
         item.classList.remove("hidden");
